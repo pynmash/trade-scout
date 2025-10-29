7991d9fe6a955c0d4905a12bd224b86fe661fc9f
Dan Toms <157254317+pynmash@users.noreply.github.com>
2025-10-29 18:37:37 +0000

    Add getting price from toolstation

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def get_sf_price(code):
     }
     url = f"https://www.screwfix.com/p/{code}"
     request = requests.get(url=url, headers=headers)
-    print(request.status_code)
     if request.status_code == 200:
         data = request.text
         soup = BeautifulSoup(data, "html.parser")
@@ -29,14 +28,32 @@ def get_sf_price(code):
         price_right_string = price_right.contents[2]
         price_string = f"{price_left_string}.{price_right_string}"
         return price_string
+    else:
+        print(f"Failed with status code {request.status_code}")
+        return
 
 
 def get_ts_price(code):
-    pass
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+    }
+    url = f"https://www.toolstation.com/p{code}"
+    print(url)
+    request = requests.get(url=url, headers=headers)
+    if request.status_code == 200:
+        data = request.text
+        soup = BeautifulSoup(data, "html.parser")
+        toolstation_price_unparsed = soup.find("span", class_="md:text-size-9")
+        toolstation_price = float(toolstation_price_unparsed.text.lstrip()[1:])
+        return toolstation_price
+    else:
+        print(f"Failed with status code {request.status_code}")
+        return
 
 
 def main():
-    sf_price = get_sf_price(codes["ptfe"][0])
+    # sf_price = get_sf_price(codes["ptfe"][0])
+    ts_price = get_ts_price(codes["ptfe"][1])
 
 
 if __name__ == "__main__":
