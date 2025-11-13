58ead41847373416582b074d7a094f74e98ce0f1
Dan Toms <157254317+pynmash@users.noreply.github.com>
2025-11-13 12:54:58 +0000

    Refactor code in to functions

--- db.py
+++ db.py
@@ -5,27 +5,37 @@ import db_credentials
 def insert_new_product(
     name: str, ts_code: int, sf_code: int, qty: int, pack_size: str, description: str
 ):
+    conn = psycopg2.connect(
+    database="trade-scout", host="localhost", user=db_credentials.user, password=db_credentials.password, port="5432"
+    )
+
+    cursor = conn.cursor()
+
     cursor.execute(
         "insert into products (name, ts_code, ts_price, sf_code, sf_price, qty, pack_size, description) values (%s, %s, %s, %s, %s, %s, %s, %s)",
         (name, ts_code, 0, sf_code, 0, qty, pack_size, description),
     )
 
+    conn.commit()
+    conn.close()
 
-conn = psycopg2.connect(
+
+
+def create_db():
+    conn = psycopg2.connect(
     database="trade-scout", host="localhost", user=db_credentials.user, password=db_credentials.password, port="5432"
-)
+    )
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
-cursor.execute(
-    "create table if not exists products(id serial, name varchar(255), ts_code int, ts_price int, sf_code int, sf_price int, description varchar(255));"
-)
+    cursor.execute(
+        "create table if not exists products(id serial, name varchar(255), ts_code int, ts_price int, sf_code int, sf_price int, description varchar(255));"
+    )
+    conn.commit()
+    conn.close()
 
-insert_new_product("ptfe", 63859, 35444, 1, "10", "Plumbers tape")
 
-conn.commit()
 
-cursor.execute("select * from products")
-print(cursor.fetchall())
 
-conn.close()
+# cursor.execute("select * from products")
+# print(cursor.fetchall())
