e9241c365ad31936325e09c4fd89b452efab9115
Dan Toms <157254317+pynmash@users.noreply.github.com>
2025-11-13 12:40:55 +0000

    Add new db columns

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
 import psycopg2
 
 
-def insert_new_product(name: str, ts_code: int, sf_code: int, description: str):
+def insert_new_product(
+    name: str, ts_code: int, sf_code: int, qty: int, pack_size: str, description: str
+):
     cursor.execute(
-        "insert into products (name, ts_code, ts_price, sf_code, sf_price, description) values (%s, %s, %s, %s, %s, %s)",
-        (name, ts_code, 0, sf_code, 0, description),
+        "insert into products (name, ts_code, ts_price, sf_code, sf_price, qty, pack_size, description) values (%s, %s, %s, %s, %s, %s, %s, %s)",
+        (name, ts_code, 0, sf_code, 0, qty, pack_size, description),
     )
 
 
@@ -18,7 +20,7 @@ cursor.execute(
     "create table if not exists products(id serial, name varchar(255), ts_code int, ts_price int, sf_code int, sf_price int, description varchar(255));"
 )
 
-insert_new_product("ptfe", 63859, 35444, "Plumbers tape")
+insert_new_product("ptfe", 63859, 35444, 1, "10", "Plumbers tape")
 
 conn.commit()
 
